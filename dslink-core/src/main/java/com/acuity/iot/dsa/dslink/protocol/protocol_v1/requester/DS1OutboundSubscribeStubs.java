d287060d904914661544b593036aad2bfe045166
Aaron <aaron_hansen@yahoo.com>
2017-10-23 09:29:20 -0700

    Refactor Requester

---
+++ dslink-core/src/main/java/com/acuity/iot/dsa/dslink/protocol/protocol_v1/requester/DS1OutboundSubscribeStubs.java
@@ -0,0 +1,196 @@
+package com.acuity.iot.dsa.dslink.protocol.protocol_v1.requester;
+
+import org.iot.dsa.io.DSIWriter;
+import org.iot.dsa.node.DSElement;
+import org.iot.dsa.node.DSStatus;
+import org.iot.dsa.time.DSDateTime;
+
+public class DS1OutboundSubscribeStubs {
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Fields
+    ///////////////////////////////////////////////////////////////////////////
+
+    private DS1OutboundSubscribeStub first;
+    private DS1OutboundSubscribeStub last;
+    private DSStatus lastStatus;
+    private DSDateTime lastTs;
+    private DSElement lastValue;
+    private String path;
+    private int qos = 0;
+    private Integer sid;
+    private int size;
+    private State state = State.PENDING_SUBSCRIBE;
+    private DS1OutboundSubscriptions subscriptions;
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Constructors
+    ///////////////////////////////////////////////////////////////////////////
+
+    public DS1OutboundSubscribeStubs(
+            Integer sid,
+            DS1OutboundSubscribeStub stub,
+            DS1OutboundSubscriptions subscriptions) {
+        this.sid = sid;
+        this.subscriptions = subscriptions;
+        path = stub.getRequest().getPath();
+        add(stub);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Methods
+    ///////////////////////////////////////////////////////////////////////////
+
+    void add(DS1OutboundSubscribeStub stub) {
+        if (stub.getQos() > qos) {
+            qos = stub.getQos();
+        }
+        if (contains(stub)) {
+            return;
+        }
+        if (last == null) {
+            first = stub;
+            last = stub;
+        } else {
+            last.setNext(stub);
+            last = stub;
+        }
+        if (++size > 1) {
+            if (lastValue != null) {
+                try {
+                    stub.process(lastTs, lastValue, lastStatus);
+                } catch (Exception x) {
+                    subscriptions.severe(path, x);
+                }
+            }
+        }
+    }
+
+    public void close() {
+        //TODO who calls this and for what purpose
+        /*
+        DS1OutboundSubscribeStub cur = first;
+        while (cur != null) {
+            cur.close();
+            cur = cur.getNext();
+        }
+        */
+    }
+
+    private boolean contains(DS1OutboundSubscribeStub stub) {
+        if (stub == first) {
+            return true;
+        }
+        return predecessor(stub) != last;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public int getQos() {
+        return qos;
+    }
+
+    public Integer getSid() {
+        return sid;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    /**
+     * Null if the arg is the first in the list, last if stub is not contained.
+     */
+    private DS1OutboundSubscribeStub predecessor(DS1OutboundSubscribeStub stub) {
+        if (first == null) {
+            return null;
+        }
+        if (stub == first) {
+            return null;
+        }
+        DS1OutboundSubscribeStub cur = first;
+        while (cur.getNext() != null) {
+            if (cur.getNext() == stub) {
+                return cur;
+            }
+            cur = cur.getNext();
+        }
+        return cur;
+    }
+
+    void process(DSDateTime ts, DSElement value, DSStatus status) {
+        DS1OutboundSubscribeStub stub = first;
+        while (stub != null) {
+            try {
+                stub.process(ts, value, status);
+            } catch (Exception x) {
+                subscriptions.severe(path, x);
+            }
+            stub = stub.getNext();
+        }
+        lastTs = ts;
+        lastValue = value;
+        lastStatus = status;
+    }
+
+    void remove(DS1OutboundSubscribeStub stub) {
+        if (stub == first) {
+            if (first == last) {
+                first = last = null;
+            } else {
+                first = first.getNext();
+            }
+            size--;
+        }
+        DS1OutboundSubscribeStub pred = predecessor(stub);
+        if (pred == last) { //not contained
+            return;
+        }
+        if (stub == last) {
+            last = pred;
+            pred.setNext(null);
+        }
+    }
+
+    void setState(State state) {
+        this.state = state;
+    }
+
+    int size() {
+        return size;
+    }
+
+    public void write(DSIWriter out) {
+        /*
+        out.beginMap();
+        out.key("rid").value(getRequestId());
+        out.key("method").value("subscribe");
+        Iterator<OutboundSubscription> it = request.getPath();
+        DSList paths = new DSList();
+        while (it.hasNext()) {
+            OutboundSubscription sub = it.next();
+            DSMap m = paths.addMap().put("path", sub.getPath()).put("sid", sub.getSubscriptionId());
+            Integer qos = sub.getQos();
+            if (qos != null) {
+                m.put("qos", qos);
+            }
+        }
+        out.key("paths").value(paths);
+        out.endMap();
+        */
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Inner Classes
+    ///////////////////////////////////////////////////////////////////////////
+
+    public enum State {
+        PENDING_SUBSCRIBE,
+        PENDING_UNSUBSCRIBE,
+        SUBSCRIBED,
+        UNSUBSCRIBED
+    }
+
+}
