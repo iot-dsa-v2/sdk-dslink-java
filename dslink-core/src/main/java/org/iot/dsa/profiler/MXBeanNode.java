1f9203206866b5863fd75fde8edc307bb3ac1c9f
Daniel Shapiro <d.shapiro@berkeley.edu>
2018-04-19 14:59:28 -0700

    add classes from "selfprofiler" POC DSLink

---
+++ dslink-core/src/main/java/org/iot/dsa/profiler/MXBeanNode.java
@@ -0,0 +1,83 @@
+package org.iot.dsa.profiler;
+
+import java.lang.management.PlatformManagedObject;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+
+import org.iot.dsa.node.DSIObject;
+import org.iot.dsa.node.DSInfo;
+import org.iot.dsa.node.DSNode;
+import org.iot.dsa.node.DSString;
+import org.iot.dsa.node.action.ActionInvocation;
+import org.iot.dsa.node.action.ActionResult;
+import org.iot.dsa.node.action.DSAction;
+
+public abstract class MXBeanNode extends DSNode {
+	
+	@Override
+	protected void declareDefaults() {
+		super.declareDefaults();
+		DSAction act = new DSAction() {
+			@Override
+			 public ActionResult invoke(DSInfo info, ActionInvocation invocation) {
+				((MXBeanNode) info.getParent()).refresh();
+				return null;
+			}
+    	};
+    	declareDefault("Refresh", act);
+	}
+	
+	@Override
+    protected void onStable() {
+		setupMXBean();
+    	refresh();
+    }
+	
+	private void refresh() {
+		refreshImpl();
+		discover();
+	}
+	
+	public abstract void setupMXBean();
+	
+	public abstract void refreshImpl();
+	
+	public abstract PlatformManagedObject getMXBean();
+	
+	public abstract Class<? extends PlatformManagedObject> getMXInterface();
+	
+	public abstract List<String> getOverriden();
+	
+	public void discover() {
+		PlatformManagedObject bean = getMXBean();
+		Class<? extends PlatformManagedObject> clazz = getMXInterface();
+		for (Method meth: clazz.getMethods()) {
+			String methName = meth.getName();
+			if (meth.getParameterCount() == 0 &&
+					meth.getReturnType() != Void.TYPE) {
+				String name; 
+				if (methName.startsWith("get")) {
+					name = methName.substring(3);
+				} else if (methName.startsWith("is")) {
+					name = methName.substring(2);
+				} else {
+					continue;
+				}
+				if (!name.isEmpty() && !getOverriden().contains(name)) {
+					try {
+						Object o = meth.invoke(bean);
+						putProp(name, o != null ? ProfilerUtils.objectToDSIValue(o) : DSString.EMPTY);
+					} catch (Exception e) {
+						warn(e);
+					}
+				}
+			}
+		}
+	}
+	
+	protected void putProp(String name, DSIObject obj) {
+		put(name, obj).setReadOnly(true).setTransient(true);
+	}
+
+}
---
+++ dslink-core/src/main/java/org/iot/dsa/profiler/MemoryManagerNode.java
@@ -0,0 +1,49 @@
+package org.iot.dsa.profiler;
+
+import java.lang.management.MemoryManagerMXBean;
+import java.lang.management.PlatformManagedObject;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemoryManagerNode extends MXBeanNode {
+	
+	private MemoryManagerMXBean mxbean;
+
+	public MemoryManagerNode() {
+	}
+	
+	public MemoryManagerNode(MemoryManagerMXBean mxbean) {
+		this.mxbean = mxbean;
+	}
+
+	@Override
+	public void setupMXBean() {
+		if (mxbean == null) {
+			getParent().remove(getInfo());
+		}
+	}
+
+	@Override
+	public void refreshImpl() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public PlatformManagedObject getMXBean() {
+		return mxbean;
+	}
+
+	@Override
+	public Class<? extends PlatformManagedObject> getMXInterface() {
+		return MemoryManagerMXBean.class;
+	}
+	
+	private static List<String> overriden = new ArrayList<String>();
+	
+	@Override
+	public List<String> getOverriden() {
+		return overriden;
+	}
+
+}
