22d5c2ae7b3028a5a5df8e0b581df416c1883567
Aaron <aaron_hansen@yahoo.com>
2018-06-06 15:22:13 -0700

    Debugging

--- dslink-java-v2-poc/src/main/java/org/iot/dsa/dslink/poc/MainNode.java
+++ dslink-java-v2-poc/src/main/java/org/iot/dsa/dslink/poc/MainNode.java
@@ -96,7 +96,8 @@ public class MainNode extends DSMainNode implements Runnable {
 
         @Override
         public void onUpdate(String name, DSElement value) {
-            System.out.println("list update " + name);
+            System.out.print(name);
+            System.out.print(": ");
             System.out.println(String.valueOf(value));
         }
 
