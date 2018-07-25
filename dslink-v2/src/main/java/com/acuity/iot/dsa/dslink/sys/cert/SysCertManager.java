6d077e655bd504a38fc2b5fbc5c3142b755ee5af
Daniel Shapiro <d.shapiro@berkeley.edu>
2018-07-25 16:14:21 -0700

    check CRLs when verifying a cert; add action that generates a CSR

--- dslink-v2/src/main/java/com/acuity/iot/dsa/dslink/sys/cert/SysCertManager.java
+++ dslink-v2/src/main/java/com/acuity/iot/dsa/dslink/sys/cert/SysCertManager.java
@@ -1,13 +1,41 @@
 package com.acuity.iot.dsa.dslink.sys.cert;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.security.cert.CertificateEncodingException;
 import java.security.cert.X509Certificate;
+import java.time.format.ResolverStyle;
+import java.util.Iterator;
+import javax.security.auth.x500.X500Principal;
+import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+import org.bouncycastle.pkcs.PKCS10CertificationRequest;
+import org.bouncycastle.pkcs.PKCS10CertificationRequestBuilder;
+import org.bouncycastle.pkcs.jcajce.JcaPKCS10CertificationRequestBuilder;
 import org.iot.dsa.node.DSBool;
+import org.iot.dsa.node.DSIValue;
 import org.iot.dsa.node.DSInfo;
+import org.iot.dsa.node.DSMap;
 import org.iot.dsa.node.DSNode;
 import org.iot.dsa.node.DSString;
+import org.iot.dsa.node.DSValueType;
+import org.iot.dsa.node.action.ActionInvocation;
+import org.iot.dsa.node.action.ActionResult;
+import org.iot.dsa.node.action.ActionSpec;
+import org.iot.dsa.node.action.ActionSpec.ResultType;
+import org.iot.dsa.node.action.ActionValues;
+import org.iot.dsa.node.action.DSAbstractAction;
+import org.iot.dsa.node.action.DSAction;
+import org.iot.dsa.node.action.DSActionValues;
 import org.iot.dsa.security.DSPasswordAes128;
+import org.iot.dsa.util.DSException;
 
 /**
  * Certificate management for the whole process.  This is basically a stub for future
@@ -28,6 +56,7 @@ public class SysCertManager extends DSNode {
     private static final String CERTFILE_TYPE = "Cert_File_Type";
     private static final String LOCAL_TRUSTSTORE = "Local_Truststore";
     private static final String QUARANTINE = "Quarantine";
+    private static final String GENERATE_CSR = "Generate_Certificate_Signing_Request";
 
     // Fields
     // ------
@@ -37,6 +66,7 @@ public class SysCertManager extends DSNode {
     private DSInfo keystore = getInfo(CERTFILE);
     private DSInfo keystorePass = getInfo(CERTFILE_PASS);
     private DSInfo keystoreType = getInfo(CERTFILE_TYPE);
+    private DSInfo generateCSR = getInfo(GENERATE_CSR);
     private CertCollection localTruststore; 
     private CertCollection quarantine;
 
@@ -80,6 +110,24 @@ public class SysCertManager extends DSNode {
         declareDefault(CERTFILE_PASS, DSPasswordAes128.valueOf("dsarocks"));
         declareDefault(LOCAL_TRUSTSTORE, new CertCollection());
         declareDefault(QUARANTINE, new CertCollection()).setTransient(true);
+        declareDefault(GENERATE_CSR, getGenerateCSRAction());
+    }
+    
+    private DSAbstractAction getGenerateCSRAction() {
+        DSAbstractAction act = new DSAbstractAction() {
+            
+            @Override
+            public void prepareParameter(DSInfo info, DSMap parameter) {
+            }
+            
+            @Override
+            public ActionResult invoke(DSInfo info, ActionInvocation invocation) {
+                return ((SysCertManager) info.getParent()).generateCSR(info);
+            }
+        };
+        act.setResultType(ResultType.VALUES);
+        act.addValueResult("CSR", DSValueType.STRING);
+        return act;
     }
 
     private String getCertFilePass() {
@@ -150,5 +198,44 @@ public class SysCertManager extends DSNode {
         getLocalTruststore().addCertificate(name, certStr);
     }
     
+    private ActionResult generateCSR(DSInfo actionInfo) {
+        KeyPairGenerator keyGen;
+        try {
+            keyGen = KeyPairGenerator.getInstance("RSA");
+        } catch (NoSuchAlgorithmException e) {
+            DSException.throwRuntime(e);
+            return null;
+        }
+        keyGen.initialize(2048, new SecureRandom());
+        KeyPair pair = keyGen.generateKeyPair();
+        PKCS10CertificationRequestBuilder p10Builder = new JcaPKCS10CertificationRequestBuilder(
+            new X500Principal("CN=dslink-java-v2"), pair.getPublic());
+        JcaContentSignerBuilder csBuilder = new JcaContentSignerBuilder("SHA256withRSA");
+        ContentSigner signer;
+        try {
+            signer = csBuilder.build(pair.getPrivate());
+        } catch (OperatorCreationException e) {
+            DSException.throwRuntime(e);
+            return null;
+        }
+        PKCS10CertificationRequest csr = p10Builder.build(signer);
+        StringWriter str = new StringWriter();
+        JcaPEMWriter pemWriter = new JcaPEMWriter(str);
+        try {
+            pemWriter.writeObject(csr);
+        } catch (IOException e) {
+            DSException.throwRuntime(e);
+            return null;
+        } finally {
+            try {
+                pemWriter.close();
+                str.close();
+            } catch (IOException e) {
+                DSException.throwRuntime(e);
+                return null;
+            }
+        }
+        return new DSActionValues(actionInfo.getAction()).addResult(DSString.valueOf(str));
+    }
 
 }
