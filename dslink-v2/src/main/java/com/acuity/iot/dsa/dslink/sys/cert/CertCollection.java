c25c0393e6df6ce58129e248a4deb68cf9ee6123
Daniel Shapiro <d.shapiro@berkeley.edu>
2019-06-04 14:55:51 -0700

    add ability to manually import certs to truststore

--- dslink-v2/src/main/java/com/acuity/iot/dsa/dslink/sys/cert/CertCollection.java
+++ dslink-v2/src/main/java/com/acuity/iot/dsa/dslink/sys/cert/CertCollection.java
@@ -1,17 +1,31 @@
 package com.acuity.iot.dsa.dslink.sys.cert;
 
+import java.io.ByteArrayInputStream;
 import java.security.cert.CertificateEncodingException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.util.Base64;
 import java.util.Base64.Encoder;
 import org.iot.dsa.node.DSIObject;
+import org.iot.dsa.node.DSInfo;
+import org.iot.dsa.node.DSMap;
+import org.iot.dsa.node.DSMetadata;
 import org.iot.dsa.node.DSNode;
+import org.iot.dsa.node.DSValueType;
+import org.iot.dsa.node.action.ActionInvocation;
+import org.iot.dsa.node.action.ActionResult;
+import org.iot.dsa.node.action.DSAction;
 import org.iot.dsa.time.DSTime;
+import org.iot.dsa.util.DSException;
 
 /**
  * @author Daniel Shapiro
  */
 public class CertCollection extends DSNode {
+    
+    private static final String ADD_CERT = "Add Certificate";
+    private static final String CERT = "Certificate";
 
     public void addCertificate(X509Certificate cert) throws CertificateEncodingException {
         String name = certToName(cert);
@@ -45,6 +59,37 @@ public class CertCollection extends DSNode {
             throws CertificateEncodingException {
         Encoder encoder = Base64.getEncoder();
         return encoder.encodeToString(cert.getEncoded());
+    }
+    
+    @Override
+    protected void declareDefaults() {
+        super.declareDefaults();
+        declareDefault(ADD_CERT, makeAddCertAction());
+    }
+    
+    private DSAction makeAddCertAction() {
+        DSAction act = new DSAction.Parameterless() {
+            
+            @Override
+            public ActionResult invoke(DSInfo target, ActionInvocation request) {
+                ((CertCollection) target.get()).addCert(request.getParameters());
+                return null;
+            }
+        };
+        act.addParameter(CERT, DSValueType.STRING, null).setEditor(DSMetadata.STR_EDITOR_TEXT_AREA);
+        return act;
+    }
+    
+    private void addCert(DSMap parameters) {
+        String certStr = parameters.getString(CERT);
+        try {
+            CertificateFactory cFactory = CertificateFactory.getInstance("X.509");
+            X509Certificate cert = (X509Certificate) cFactory.generateCertificate(new ByteArrayInputStream(certStr.getBytes()));
+            addCertificate(cert);
+        } catch (CertificateException e) {
+            warn("", e);
+            DSException.throwRuntime(e);
+        }
     }
 
 }
